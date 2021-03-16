24b679ab41f4bd32490a8025c78ef697489d366f
zhuang.a.ren <zhuang.a.ren@rakuten.com>
2021-03-16 11:10:12 +0800

    Chapter: Generator and Chapter: Async/Await

---
+++ async_await.ts
@@ -0,0 +1,35 @@
+// async function returns a Promise object
+// await 意味着 等待一个Promise对象的状态变为resolved
+// consider 'await' as 'yield in Generator'
+// 'await' keyword won't RETURN any value for async function, only return keyword could
+async function f1() {
+    return 'hello world';
+    // return Promise.resolve('hello world');
+}
+
+f1().then(v => console.log(v)); // "hello world"
+// Or
+(async function () {
+    const str = await f1();
+    console.log(str); // "hello world"
+})();
+
+// ****************************************************
+async function f2() {
+    throw new Error('出错了');
+    // return Promise.reject(new Error('出错了'));
+}
+f2().catch(err => console.log(err)); // 出错了
+
+// Sleep Implementation
+function sleep(interval: number) {
+    return new Promise(resolve => {
+        setTimeout(resolve, interval);
+    });
+}
+
+(async function (interval: number) {
+    for (let i = 0; i < interval; i++) {
+        await sleep(1000);
+    }
+})(5);
--- generator.ts
+++ generator.ts
@@ -1,1 +1,27 @@
 // "yield* [1, 2, 3]" seems like "yield from [1, 2, 3]" in Python
+const myIterable: any = {};
+myIterable[Symbol.iterator] = function* () {
+    yield 1;
+    yield 2;
+    yield 3;
+};
+
+console.log([...myIterable]); // [1, 2, 3]
+
+// ****************************************************
+function* f(): Generator<number> {
+    for (let i = 0; true; i++) {
+        const reset = yield i; // 'reset' receives the arg of .next()
+        if (reset) {
+            i -= 1;
+        }
+    }
+}
+
+const g = f();
+g.next(); // { value: 0, done: false }
+g.next(); // { value: 1, done: false }
+g.next(true); // { value: 0, done: false }
+
+// g.next() equals to g.send(None) in Python
+// g.next(true) equals to g.send(true) in Python
